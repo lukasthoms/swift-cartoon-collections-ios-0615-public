b2ea6cd517e0e0cea3a3209295892b3dd6dc4e37
lukasthoms <luk@stho.ms>
2015-06-09 15:26:53 -0400

    finished lab

---
+++ CartoonCollections/CartoonCollections/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CartoonCollections
+//
+//  Created by Lukas Thoms on 6/9/15.
+//  Copyright (c) 2015 Lukas Thoms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
